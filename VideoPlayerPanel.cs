01670b06248cec5bf7312b5961bfd7d1abde1c54
unknown <hallc3@NHQIT-W74177920.usa.glb.tru.com>
2015-01-13 18:39:02 -0500

    Clean up code that sets panel size

--- VideoPlayerPanel.cs
+++ VideoPlayerPanel.cs
@@ -17,6 +17,7 @@ namespace VideoTracker
         public VideoSeries videoSeries;
         public VideoTrackerData videoTrackerData;
         public VideoTrackerForm videoTrackerForm;
+        public int initialWidth;
 
         public VideoPlayerPanel()
         {
@@ -92,42 +93,47 @@ namespace VideoTracker
                 playNextButton.Enabled = false;
                 playButton.Enabled = false;
                 seriesName.Text = videoSeries.title; // Title only, no episode number available.
-                PerformLayout();
-                ResumeLayout();
-                return;
-            }
-
-            VideoFile v = videoSeries.currentVideo;
-            if (v.postseason == 1)
-            {
-                seriesName.Text = videoSeries.title + " Season: " + v.season + " Special: " + v.episode;
             }
             else
             {
-                seriesName.Text = videoSeries.title + " Season: " + v.season + " Episode: " + v.episode;
-            }
-            int index = videoSeries.videoFiles.IndexOfKey(v.key);
-            videoSelector.SelectedIndex = index; 
-            if (index == 0) {
-                backButton.Enabled = false;
-            } else {
-                backButton.Enabled = true;
-            }
 
-            if (index == videoSeries.videoFiles.Count - 1)
-            {
-                playNextButton.Enabled = false;
-                nextButton.Enabled = false;
-            }
-            else
-            {
-                playNextButton.Enabled = true;
-                nextButton.Enabled = true;
-            }
+                VideoFile v = videoSeries.currentVideo;
+                if (v.postseason == 1)
+                {
+                    seriesName.Text = videoSeries.title + " Season: " + v.season + " Special: " + v.episode;
+                }
+                else
+                {
+                    seriesName.Text = videoSeries.title + " Season: " + v.season + " Episode: " + v.episode;
+                }
+                int index = videoSeries.videoFiles.IndexOfKey(v.key);
+                videoSelector.SelectedIndex = index;
+                if (index == 0)
+                {
+                    backButton.Enabled = false;
+                }
+                else
+                {
+                    backButton.Enabled = true;
+                }
 
-            videoTrackerForm.CheckAutoSave();
+                if (index == videoSeries.videoFiles.Count - 1)
+                {
+                    playNextButton.Enabled = false;
+                    nextButton.Enabled = false;
+                }
+                else
+                {
+                    playNextButton.Enabled = true;
+                    nextButton.Enabled = true;
+                }
+            }
+            flowLayoutPanel.AutoSize = true;
+            ResumeLayout(false);
             PerformLayout();
-            ResumeLayout();
+            initialWidth = flowLayoutPanel.Width;
+            flowLayoutPanel.AutoSize = false;
+            videoTrackerForm.CheckAutoSave();
         }
 
         private void editButton_Click(object sender, EventArgs e)
